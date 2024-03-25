ac6837a8fe1c0e78238dbd396c3a8053ba05d69a
Krzysztof Sychla <ksychla@antmicro.com>
2024-03-25 12:58:55 +0100

    soc: Add zynqmp cortex-R8 support
    Add zynqmp with Cortex R8
    
    Signed-off-by: Krzysztof Sychla <ksychla@antmicro.com>

---
+++ soc/xlnx/zynqmp_r8/soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019 Lexmark International, Inc.
+ * Copyright (c) 2024 Antmicro <www.antmicro.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef ZEPHYR_SOC_XLNX_ZYNQMP_R8_SOC_H_
+#define ZEPHYR_SOC_XLNX_ZYNQMP_R8_SOC_H_
+
+/* Define CMSIS configurations */
+#define __CR_REV		1U
+
+#define __GIC_PRESENT		0U
+#define __TIM_PRESENT		0U
+
+#endif /* ZEPHYR_SOC_XLNX_ZYNQMP_R8_SOC_H_ */
